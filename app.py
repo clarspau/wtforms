3bda547cfbd5182c57f818c548cce753ec28f412
clarspau <125636195+clarspau@users.noreply.github.com>
2023-11-13 21:36:51 -0800

    Modified the app.py to add routes and functions for forms to add and edit pet info. Created html templates for add_pet_form and edit_pet_form.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Pet adoption flask app."""
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, url_for, jsonify
 
 # commenting out the DebugToolbar cause it's causing error
 # from flask_debugtoolbar import DebugToolbarExtension
@@ -28,3 +28,44 @@ def pets_page():
 
     pets = Pet.query.all()
     return render_template("pets_page.html", pets=pets)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """Add a new pet."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        data = {key: value for key, value in form.data.items()
+                if key != "csrf_token"}
+     #    Pet(**data) is a shortcut syntax to get info such as form.name.data, form.age.data, etc.
+        new_pet = Pet(**data)
+
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{new_pet.name} has been added!")
+        return redirect(url_for('pets_page'))
+
+    else:
+        return render_template("add_pet_form.html", form=form)
+
+
+@app.route("/<int:pet_db>", methods=["GET", "POST"])
+def edit_pet(pet_id):
+    """Edit a pet's info."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+
+        db.session.commit()
+        flash(f"{pet.name} has been updated!")
+        return redirect(url_for('pets_page'))
+
+    else:
+        return render_template("edit_pet_form.html", pet=pet, form=form)
